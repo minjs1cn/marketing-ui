781e12ea1a887cf2c483764a14bb51ce88fcc87b
minjs1cn <303890562@qq.com>
2021-07-06 16:27:23 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
-console.log(process.env.NODE_ENV)
-const isDev = process.env.NODE_ENV === 'development'
 
 export default defineConfig({
   server: {
@@ -18,5 +16,5 @@ export default defineConfig({
       'marketing-ui': path.resolve(__dirname, 'es'),
     }
   },
-  base: '/marketing-ui/demo'
+  base: '/marketing-ui/'
 })
